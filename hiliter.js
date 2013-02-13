bfbdce509ea5611f9195d99c9c70439bde058fa6
Senthil Kumar <senthilkumar@thoughtworks.com>
2013-02-13 23:10:18 +0530

    Reformat document - Senthil

--- hiliter.js
+++ hiliter.js
@@ -1,185 +1,186 @@
-var Hiliter = (function () {
-var getNonHighlightAncestorContainer = function(range) {
-	var commonAncestor = range.commonAncestorContainer;
-	while(commonAncestor.nodeName === "#text" || commonAncestor.getAttribute("data-highlight-id")) {
-		commonAncestor = commonAncestor.parentElement;
-	}
-	return commonAncestor;
-};
-
-var createWrapperWithIdentifier = function(identifier, type) {
-    var element = document.createElement('span');
-    element.setAttribute('data-identifier', type + "_" + identifier);
-    return element;
-};
-
-var wrapElementFromOffset = function(data) {
-    var newRange = document.createRange();
-    newRange.setStart(data.element, data.startOffset);
-    newRange.setEnd(data.element, data.endOffset);
-    newRange.surroundContents(data.wrapper);
-};
-
-var calculateOffsetTill = function(container, endIndex) {
-    var insideTag = false;
-    var index = 1;
-    for(var i=0; i<=endIndex; i++) {
-	if(container[i] === '<') {
-	        insideTag = true;
-	    }
-	index += (insideTag) ? 0 : 1;  
-	if(container[i] === '>') {
-	        insideTag = false;
-	    }
-    }
-    return index;
-};
-
-var offsetFromContainer = function(content, instance) {
-    var startOffset = content.indexOf("<span data-identifier=\"start_" + instance + "\"")
-    var endOffset = content.indexOf("<span data-identifier=\"end_" + instance + "\"")
-    endOffset = content.indexOf("</span>", endOffset);
-    return {
-	startOffset: calculateOffsetTill(content, startOffset),
-	endOffset: calculateOffsetTill(content, endOffset)
-    };
-};
-
-var convertTextOffsetToDocumentOffset = function(content, offset) {
-    var insideTag = false;
-    var index = 0, i = 0;
-    for(i=0; i< content.length && (index != offset); i++) {
-	if(content[i] === '<') {
-	   insideTag = true;
-	}
-	index += (insideTag) ? 0 : 1;  	
-	if(content[i] === '>') {
-	   insideTag = false;
-	}
-    }
-    return i;
-};
-
-var sanitize = function(content, id) {
-	var regex = new RegExp("(<span[^>]+data-highlight-id\\s*=\\s*(\"|')" + id + "\\2[^>]*>)(\\s*)(</span>)",'g');
-	return content.replace(regex, '');
-};
-
-var highlightTagWithId = function(id, className) {
-	return "<span data-highlight-id=\"" + id + "\" class=\"" + className + "\">";
-};
-
-var addHighlight = function(content, highlight) {
-    var nodeContent = content.innerHTML;
-    var startOffset = convertTextOffsetToDocumentOffset(nodeContent, highlight.startOffset);
-    var endOffset = convertTextOffsetToDocumentOffset(nodeContent, highlight.endOffset - 1);
-    var htmlElement = nodeContent.substring(0, startOffset - 1) + highlightTagWithId(highlight.id, highlight.highlightClass);
-    for(var i = startOffset - 1; i < endOffset; i++) {
-	    htmlElement += nodeContent[i];
-	    if(nodeContent[i] === '<')
-	        htmlElement += "/span><";
-	    if(nodeContent[i] === '>')
-	        htmlElement += highlightTagWithId(highlight.id, highlight.highlightClass);
-    }
-    htmlElement += "</span>";
-    content.innerHTML = sanitize(htmlElement, highlight.id) + nodeContent.substring(endOffset);
-};
-
-var findNodePosition = function(data) {
-	var filter = function(node) {	    
-		if (node.getAttribute("data-highlight-id") === null && node.getAttribute("data-identifier") == null) return NodeFilter.FILTER_ACCEPT;
-		return NodeFilter.FILTER_SKIP;
+var Hiliter = (function() {
+	var getNonHighlightAncestorContainer = function(range) {
+		var commonAncestor = range.commonAncestorContainer;
+		while (commonAncestor.nodeName === "#text" || commonAncestor.getAttribute("data-highlight-id")) {
+			commonAncestor = commonAncestor.parentElement;
+		}
+		return commonAncestor;
 	};
-	var node, index = 0;
-	var nodes = document.createNodeIterator(data.content.querySelector(data.relativeTo), NodeFilter.SHOW_ELEMENT, filter, false);	
-	while((node = nodes.nextNode()) != null) {
-	    index ++;
-		if(node == data.nodeToFind) break;
-	}
-	return index;
-};
 
-var findNodeByPosition = function(data){
-	var filter = function(node) {	    
-		if (node.getAttribute("data-highlight-id") === null && node.getAttribute("data-identifier") == null) return NodeFilter.FILTER_ACCEPT;
-		return NodeFilter.FILTER_SKIP;
+	var createWrapperWithIdentifier = function(identifier, type) {
+		var element = document.createElement('span');
+		element.setAttribute('data-identifier', type + "_" + identifier);
+		return element;
 	};
-	var index = 0;
-	var relativeTo = document.querySelector(data.relativeTo);
-	var nodes = document.createNodeIterator((relativeTo)?relativeTo:data.content, NodeFilter.SHOW_ELEMENT, filter, false);
-	while((node = nodes.nextNode())!= null){
-		index++;
-		if(index == data.nodePosition)
-		return node;
-	}		
-};
 
-var wrapSelectionWithDifferentParents = function(range, instanceId) {
-	wrapElementFromOffset({
-		element: range.startContainer, 
-		startOffset: range.startOffset, 
-		endOffset: range.startContainer.length,
-		wrapper: createWrapperWithIdentifier(instanceId, "start")
-	});
-	wrapElementFromOffset({
-		element: range.endContainer, 
-		startOffset: 0, 
-		endOffset: range.endOffset,
-		wrapper: createWrapperWithIdentifier(instanceId, "end")
-	});	
-};
-
-var wrapSelectionWithSameParent = function(range, instanceId) {
-	wrapElementFromOffset({
-		element: range.startContainer, 
-		startOffset: range.startOffset, 
-		endOffset: range.startOffset,
-		wrapper: createWrapperWithIdentifier(instanceId, "start")
-	});
-	wrapElementFromOffset({
-		element: range.endContainer, 
-		startOffset: range.endOffset, 
-		endOffset: range.endOffset,
-		wrapper: createWrapperWithIdentifier(instanceId, "end")
-	});		
-};
-
-var isSelectionWithinSameParent = function(range) {
-	return range.startContainer == range.endContainer;
-};
-
-var highlight = function (containerSelector, className, highlightId) {
-    var range = window.getSelection().getRangeAt(0);
-    var highlightId = (highlightId) ? highlightId : (new Date().getTime());
-	isSelectionWithinSameParent(range) ? wrapSelectionWithSameParent(range, highlightId) : wrapSelectionWithDifferentParents(range, highlightId);		
-    var commonAncestor = getNonHighlightAncestorContainer(range);
-    var offset = offsetFromContainer(commonAncestor.innerHTML, highlightId);
-	var highlightData = {
-		id: highlightId,
-		commonAncestorPosition: findNodePosition({
-			nodeToFind: commonAncestor,
-			content: document,
-			relativeTo: containerSelector,
-			highlightClass: className}),
-		startOffset: offset.startOffset,
-		endOffset: offset.endOffset,
-		highlightClass: className
+	var wrapElementFromOffset = function(data) {
+		var newRange = document.createRange();
+		newRange.setStart(data.element, data.startOffset);
+		newRange.setEnd(data.element, data.endOffset);
+		newRange.surroundContents(data.wrapper);
 	};
-	addHighlight(commonAncestor, highlightData);
-	return highlightData;
-};
 
-var loadHighlights = function(containerSelector, highlights) {
-	for(var i = 0;i<highlights.length;i++) {
-		var commonAncestor = findNodeByPosition({
-												nodePosition: highlights[i].commonAncestorPosition,
-												content: document.body,
-												relativeTo: containerSelector,
-												highlightClass: highlights[i].highlightClass
-											  });
-		addHighlight(commonAncestor, highlights[i]);
-	}
-};
+	var calculateOffsetTill = function(container, endIndex) {
+		var insideTag = false;
+		var index = 1;
+		for (var i = 0; i <= endIndex; i++) {
+			if (container[i] === '<') {
+				insideTag = true;
+			}
+			index += (insideTag) ? 0 : 1;
+			if (container[i] === '>') {
+				insideTag = false;
+			}
+		}
+		return index;
+	};
+
+	var offsetFromContainer = function(content, instance) {
+		var startOffset = content.indexOf("<span data-identifier=\"start_" + instance + "\"")
+		var endOffset = content.indexOf("<span data-identifier=\"end_" + instance + "\"")
+		endOffset = content.indexOf("</span>", endOffset);
+		return {
+			startOffset: calculateOffsetTill(content, startOffset),
+			endOffset: calculateOffsetTill(content, endOffset)
+		};
+	};
+
+	var convertTextOffsetToDocumentOffset = function(content, offset) {
+		var insideTag = false;
+		var index = 0,
+			i = 0;
+		for (i = 0; i < content.length && (index != offset); i++) {
+			if (content[i] === '<') {
+				insideTag = true;
+			}
+			index += (insideTag) ? 0 : 1;
+			if (content[i] === '>') {
+				insideTag = false;
+			}
+		}
+		return i;
+	};
+
+	var sanitize = function(content, id) {
+		var regex = new RegExp("(<span[^>]+data-highlight-id\\s*=\\s*(\"|')" + id + "\\2[^>]*>)(\\s*)(</span>)", 'g');
+		return content.replace(regex, '');
+	};
+
+	var highlightTagWithId = function(id, className) {
+		return "<span data-highlight-id=\"" + id + "\" class=\"" + className + "\">";
+	};
+
+	var addHighlight = function(content, highlight) {
+		var nodeContent = content.innerHTML;
+		var startOffset = convertTextOffsetToDocumentOffset(nodeContent, highlight.startOffset);
+		var endOffset = convertTextOffsetToDocumentOffset(nodeContent, highlight.endOffset - 1);
+		var htmlElement = nodeContent.substring(0, startOffset - 1) + highlightTagWithId(highlight.id, highlight.highlightClass);
+		for (var i = startOffset - 1; i < endOffset; i++) {
+			htmlElement += nodeContent[i];
+			if (nodeContent[i] === '<') htmlElement += "/span><";
+			if (nodeContent[i] === '>') htmlElement += highlightTagWithId(highlight.id, highlight.highlightClass);
+		}
+		htmlElement += "</span>";
+		content.innerHTML = sanitize(htmlElement, highlight.id) + nodeContent.substring(endOffset);
+	};
+
+	var findNodePosition = function(data) {
+		var filter = function(node) {
+			if (node.getAttribute("data-highlight-id") === null && node.getAttribute("data-identifier") == null) return NodeFilter.FILTER_ACCEPT;
+			return NodeFilter.FILTER_SKIP;
+		};
+		var node, index = 0;
+		var nodes = document.createNodeIterator(data.content.querySelector(data.relativeTo), NodeFilter.SHOW_ELEMENT, filter, false);
+		while ((node = nodes.nextNode()) != null) {
+			index++;
+			if (node == data.nodeToFind) break;
+		}
+		return index;
+	};
+
+	var findNodeByPosition = function(data) {
+		var filter = function(node) {
+			if (node.getAttribute("data-highlight-id") === null && node.getAttribute("data-identifier") == null) return NodeFilter.FILTER_ACCEPT;
+			return NodeFilter.FILTER_SKIP;
+		};
+		var index = 0;
+		var relativeTo = document.querySelector(data.relativeTo);
+		var nodes = document.createNodeIterator((relativeTo) ? relativeTo : data.content, NodeFilter.SHOW_ELEMENT, filter, false);
+		while ((node = nodes.nextNode()) != null) {
+			index++;
+			if (index == data.nodePosition) return node;
+		}
+	};
+
+	var wrapSelectionWithDifferentParents = function(range, instanceId) {
+		wrapElementFromOffset({
+			element: range.startContainer,
+			startOffset: range.startOffset,
+			endOffset: range.startContainer.length,
+			wrapper: createWrapperWithIdentifier(instanceId, "start")
+		});
+		wrapElementFromOffset({
+			element: range.endContainer,
+			startOffset: 0,
+			endOffset: range.endOffset,
+			wrapper: createWrapperWithIdentifier(instanceId, "end")
+		});
+	};
+
+	var wrapSelectionWithSameParent = function(range, instanceId) {
+		wrapElementFromOffset({
+			element: range.startContainer,
+			startOffset: range.startOffset,
+			endOffset: range.startOffset,
+			wrapper: createWrapperWithIdentifier(instanceId, "start")
+		});
+		wrapElementFromOffset({
+			element: range.endContainer,
+			startOffset: range.endOffset,
+			endOffset: range.endOffset,
+			wrapper: createWrapperWithIdentifier(instanceId, "end")
+		});
+	};
+
+	var isSelectionWithinSameParent = function(range) {
+		return range.startContainer == range.endContainer;
+	};
+
+	var highlight = function(containerSelector, className, highlightId) {
+		var range = window.getSelection()
+			.getRangeAt(0);
+		var highlightId = (highlightId) ? highlightId : (new Date()
+			.getTime());
+		isSelectionWithinSameParent(range) ? wrapSelectionWithSameParent(range, highlightId) : wrapSelectionWithDifferentParents(range, highlightId);
+		var commonAncestor = getNonHighlightAncestorContainer(range);
+		var offset = offsetFromContainer(commonAncestor.innerHTML, highlightId);
+		var highlightData = {
+			id: highlightId,
+			commonAncestorPosition: findNodePosition({
+				nodeToFind: commonAncestor,
+				content: document,
+				relativeTo: containerSelector,
+				highlightClass: className
+			}),
+			startOffset: offset.startOffset,
+			endOffset: offset.endOffset,
+			highlightClass: className
+		};
+		addHighlight(commonAncestor, highlightData);
+		return highlightData;
+	};
+
+	var loadHighlights = function(containerSelector, highlights) {
+		for (var i = 0; i < highlights.length; i++) {
+			var commonAncestor = findNodeByPosition({
+				nodePosition: highlights[i].commonAncestorPosition,
+				content: document.body,
+				relativeTo: containerSelector,
+				highlightClass: highlights[i].highlightClass
+			});
+			addHighlight(commonAncestor, highlights[i]);
+		}
+	};
 	return {
 		loadHighlights: loadHighlights,
 		highlight: highlight,
--- tests/specs/hiliter_spec.js
+++ tests/specs/hiliter_spec.js
@@ -1,90 +1,99 @@
 describe("Highlighter", function() {
-    describe("Offset", function() {
+	describe("Offset", function() {
 		it("should calculate text offset from given container", function(done) {
-		    var text = "<div>The <span data-identifier=\"start_12345678\">quick</span> brown fox <span>jumps <span data-identifier=\"end_12345678\">over</span> the lazy dog</span>. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.</div>";
-		    var offsets = Hiliter.offsetFromContainer(text, "12345678");
-		    expect(offsets.startOffset).to.equal(5);
-		    expect(offsets.endOffset).to.equal(31);
+			var text = "<div>The <span data-identifier=\"start_12345678\">quick</span> brown fox <span>jumps <span data-identifier=\"end_12345678\">over</span> the lazy dog</span>. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.</div>";
+			var offsets = Hiliter.offsetFromContainer(text, "12345678");
+			expect(offsets.startOffset)
+				.to.equal(5);
+			expect(offsets.endOffset)
+				.to.equal(31);
 			done();
 		});
-    });
+	});
 	describe("Add Highlight", function() {
-	    it("should remove highlight tags with no text in them", function(done) {
+		it("should remove highlight tags with no text in them", function(done) {
 			var doc = "<div>The quick brown fox <span class=\"highlight\" data-highlight-id=\"1\"></span><span data-highlight-id=\"1\" class=\"highlight\"></span><span data-highlight-id=\"1\" class=\"highlight\">jumps over the lazy</span> dog.</div>";
 			var sanitized_markups = Hiliter.sanitize(doc, "1");
-	        expect(sanitized_markups).to.equal("<div>The quick brown fox <span data-highlight-id=\"1\" class=\"highlight\">jumps over the lazy</span> dog.</div>");
-	        done();
-	    });		
-		
+			expect(sanitized_markups)
+				.to.equal("<div>The quick brown fox <span data-highlight-id=\"1\" class=\"highlight\">jumps over the lazy</span> dog.</div>");
+			done();
+		});
+
 		it("should transform text offset to document offset", function(done) {
-		    var doc = '<div>Hello World.</div>';
-		    var documentOffset = Hiliter.convertTextOffsetToDocumentOffset(doc, 6);	    
-		    expect(documentOffset).to.equal(11);
+			var doc = '<div>Hello World.</div>';
+			var documentOffset = Hiliter.convertTextOffsetToDocumentOffset(doc, 6);
+			expect(documentOffset)
+				.to.equal(11);
 			done();
 		});
 
 		it("should add the highlight tag for the give text range", function(done) {
-		    var doc = $("<div>Hello World. Some more text here.</div>")[0];
-		    Hiliter.addHighlight(doc, {
+			var doc = $("<div>Hello World. Some more text here.</div>")[0];
+			Hiliter.addHighlight(doc, {
 				id: 1,
 				startOffset: 7,
 				endOffset: 12,
 				highlightClass: 'highlight'
-		    });
-		    expect(doc.innerHTML).to.equal('Hello <span data-highlight-id=\"1\" class=\"highlight\">World</span>. Some more text here.');
+			});
+			expect(doc.innerHTML)
+				.to.equal('Hello <span data-highlight-id=\"1\" class=\"highlight\">World</span>. Some more text here.');
 			done();
 		});
 
-		it("should not add empty highlight spans for nested tags", function(done){
-		   var doc = $("<div><div>Lorem ipsum dolor<br/></div> sit <div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.</div>")[0];
-		   Hiliter.addHighlight(doc, {
-			   id: 1,
-			   startOffset: 2,
-			   endOffset: 23,
-			   highlightClass: 'highlight'
-		   });
-		   expect(doc.innerHTML).to.equal("<div>L<span data-highlight-id=\"1\" class=\"highlight\">orem ipsum dolor</span><br></div><span data-highlight-id=\"1\" class=\"highlight\"> sit </span><div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.");
-		   done();
-		});				
-		
-		it("should add highlight for nested tags", function(done){
-		   var doc = $("<div><div>Lorem ipsum dolor</div> sit <div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.</div>")[0];
-		   Hiliter.addHighlight(doc, {
-			   id: 1,
-			   startOffset: 2,
-			   endOffset: 23,
-			   highlightClass: 'highlight'
-		   });
-		   expect(doc.innerHTML).to.equal("<div>L<span data-highlight-id=\"1\" class=\"highlight\">orem ipsum dolor</span></div><span data-highlight-id=\"1\" class=\"highlight\"> sit </span><div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.");
-		   done();
-		});		
+		it("should not add empty highlight spans for nested tags", function(done) {
+			var doc = $("<div><div>Lorem ipsum dolor<br/></div> sit <div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.</div>")[0];
+			Hiliter.addHighlight(doc, {
+				id: 1,
+				startOffset: 2,
+				endOffset: 23,
+				highlightClass: 'highlight'
+			});
+			expect(doc.innerHTML)
+				.to.equal("<div>L<span data-highlight-id=\"1\" class=\"highlight\">orem ipsum dolor</span><br></div><span data-highlight-id=\"1\" class=\"highlight\"> sit </span><div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.");
+			done();
+		});
+
+		it("should add highlight for nested tags", function(done) {
+			var doc = $("<div><div>Lorem ipsum dolor</div> sit <div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.</div>")[0];
+			Hiliter.addHighlight(doc, {
+				id: 1,
+				startOffset: 2,
+				endOffset: 23,
+				highlightClass: 'highlight'
+			});
+			expect(doc.innerHTML)
+				.to.equal("<div>L<span data-highlight-id=\"1\" class=\"highlight\">orem ipsum dolor</span></div><span data-highlight-id=\"1\" class=\"highlight\"> sit </span><div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.");
+			done();
+		});
 	});
 	describe("Find node position from root", function() {
-		it("should find the position relative to give root" , function(done) {
+		it("should find the position relative to give root", function(done) {
 			var doc = $("<div><div id=\"root\"><div>Lorem ipsum dolor</div> sit <div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.</div><div>")[0];
 			var nodeToFind = doc.querySelector('#root>div:nth-child(2)');
 			var nodePosition = Hiliter.findNodePosition({
 				nodeToFind: nodeToFind,
 				content: doc,
 				relativeTo: "#root",
-			});			
-			expect(nodePosition).to.equal(3);
+			});
+			expect(nodePosition)
+				.to.equal(3);
 			done();
 		});
-		it("should skip the highlight element" , function(done) {
+		it("should skip the highlight element", function(done) {
 			var doc = $("<div><div id=\"root\"><div>Lorem <span data-highlight-id=\"1\" class= 'highlight'>ipsum </span>dolor</div> sit <div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.</div><div>")[0];
 			var nodeToFind = doc.querySelector('#root>div:nth-child(2)');
 			var nodePosition = Hiliter.findNodePosition({
 				nodeToFind: nodeToFind,
 				content: doc,
 				relativeTo: "#root",
-			});			
-			expect(nodePosition).to.equal(3);
+			});
+			expect(nodePosition)
+				.to.equal(3);
 			done();
-		});	
+		});
 	});
 	describe("Find node position", function() {
-		it("should give the node given the node position" , function(done) {
+		it("should give the node given the node position", function(done) {
 			var doc = $("<div><div id=\"root\"><div>Lorem <span data-highlight-id=\"1\" class= 'highlight'>ipsum </span>dolor</div> sit <div>amet, <span>consectetur <span>adipiscing elit.</span> Phasellus et </span>lectus quam,</div> in iaculis diam.</div><div>")[0];
 			var nodePosition = 4;
 			var nodeToFind = doc.querySelector('#root>div:nth-child(2)');
@@ -92,9 +101,10 @@ describe("Highlighter", function() {
 				nodePosition: nodePosition,
 				content: doc,
 				relativeTo: "#root",
-			});	
-			expect(node).to.equal(nodeToFind);	
+			});
+			expect(node)
+				.to.equal(nodeToFind);
 			done();
-		});			
-	});		
+		});
+	});
 });
